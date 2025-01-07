9d09a0d195bd87288375601e3e0b8cbc3bee778a
aniseropian <seropanani@gmail.com>
2025-01-07 16:48:37 +0300

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,88 @@
+class House:
+    """Represents a house with a name and number of floors."""
+
+    houses_history = [] #Class attribute
+
+    def __new__(cls, name, number_of_floors):
+        """Adds the name of the new house to the class history."""
+        cls.houses_history.append(name)
+        return object.__new__(cls)
+
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+          return self.number_of_floors == other
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+          return self.number_of_floors < other
+        return False
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        elif isinstance(other, int):
+          return self.number_of_floors <= other
+        return False
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        elif isinstance(other, int):
+          return self.number_of_floors > other
+        return False
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        elif isinstance(other, int):
+          return self.number_of_floors >= other
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+        return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+    def __del__(self):
+       print(f"{self.name} снесён, но он останется в истории")
+
+
+# Example usage
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Deleting objects
+del h2
+del h3
+
+print(House.houses_history)
+del h1
